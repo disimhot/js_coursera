3a26b94a22266d9aa9fdf2f37d36cfd03ce6df99
Elena Efimova <ees.elena@gmail.com>
2018-05-07 19:01:58 +0300

    Метод "добавить номер"

--- 2week/_d18ecf9e48492a7bdae2d9cf4e31f880/public/index.js
+++ 2week/_d18ecf9e48492a7bdae2d9cf4e31f880/public/index.js
@@ -10,13 +10,37 @@ var phoneBook = {
 //module.exports =
 function test(command) {
 
-    var arrayCommands = command.split(' ');
-    
+    var arrayCommand = command.split(' ');
+    var commandName = arrayCommand.shift();
+    var name = 0;
+    var phone = 1;
+
+    if (commandName === 'ADD'){
+        if(!phoneBook.hasOwnProperty(arrayCommand[name])) {
+            phoneBook[arrayCommand.shift()] = arrayCommand.shift();
+        }else{
+            phoneBook[arrayCommand.shift()] += ',' + arrayCommand.shift();
+        }
+    } else if (commandName === 'REMOVE_PHONE'){
 
 
+
+
+    } else if (commandName === 'SHOW'){
+
+        
+
+    } else return false;
+
+
+    console.log(str);
 };
 
 
-test('SHOW')
+
+test('ADD Lena 545-10-02')
 test('ADD Ivan 555-10-01,555-10-03')
-test('REMOVE_PHONE 555-20-01')
+test('ADD Ivan 555-10-02')
+test('ADD Ken 535-10-02')
+test('SHOW')
+//test('REMOVE_PHONE 555-20-01')
