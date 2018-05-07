09563618efd8eb979bf79bff70921417ffde0c29
Elena Efimova <ees.elena@gmail.com>
2018-05-07 16:08:06 +0300

    удалены задания, сделанные home

--- 2week/_d18ecf9e48492a7bdae2d9cf4e31f880/public/index.js
+++ 2week/_d18ecf9e48492a7bdae2d9cf4e31f880/public/index.js
@@ -1,10 +1,22 @@
 // Телефонная книга
-var phoneBook = {};
+var phoneBook = {
+
+};
 
 /**
  * @param {String} command
  * @returns {*} - результат зависит от команды
  */
-module.exports = function (command) {
+//module.exports =
+function test(command) {
+
+    var arrayCommands = command.split(' ');
+    
+
 
 };
+
+
+test('SHOW')
+test('ADD Ivan 555-10-01,555-10-03')
+test('REMOVE_PHONE 555-20-01')
