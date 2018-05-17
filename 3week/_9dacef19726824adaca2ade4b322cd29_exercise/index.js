b90a3ab21c724c6075626c92b591f85e06aa3882
Elena Efimova <ees.elena@gmail.com>
2018-05-17 18:52:15 +0300

    3week

---
+++ 3week/_9dacef19726824adaca2ade4b322cd29_exercise/index.js
@@ -0,0 +1,24 @@
+/**
+ * @param {String} date
+ * @returns {Object}
+ */
+//module.exports =
+//var time = date('2017-05-16 13:45')
+
+function add(number, unit) {
+
+}
+
+function subtract(number, unit) {
+
+}
+
+function test(date) {
+    var castedDate = new Date(date);
+
+    console.log(castedDate)
+};
+
+
+var time = '2017-05-16 13:45';
+test(time);
