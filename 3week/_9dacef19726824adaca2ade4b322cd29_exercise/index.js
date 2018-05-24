53497e5da7aa56b91fe1d28f374542f48913ec80
Elena Efimova <ees.elena@gmail.com>
2018-05-24 18:59:41 +0300

    begin1

--- 3week/_9dacef19726824adaca2ade4b322cd29_exercise/index.js
+++ 3week/_9dacef19726824adaca2ade4b322cd29_exercise/index.js
@@ -5,9 +5,25 @@
 //module.exports =
 //var time = date('2017-05-16 13:45')
 
-function add(number, unit) {
-
+function parseDate(number, unit){
+    var units = ['years', 'months', 'days', 'hours', 'minutes'];
+        if (number > 0 && (units.includes(unit))) {
+            return number, unit;
+        } else {
+    try{
+            throw new TypeError();
+    }catch (e){
+        console.log('Данные некоррекны');
+    }
+        }
 }
+
+
+function add(number, unit) {
+    parseDate(number, unit);
+    console.log(number, unit);
+}
+
 
 function subtract(number, unit) {
 
@@ -22,3 +38,5 @@ function test(date) {
 
 var time = '2017-05-16 13:45';
 test(time);
+
+add(2,'iuhyku');
